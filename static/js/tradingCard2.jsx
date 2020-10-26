b606ae5aa3b4f78d2cb24e7ae095733b65dcafc6
Alison Cordeiro <acordeiro@gmail.com>
2020-10-25 17:18:34 -0700

    add broken solution

---
+++ static/js/tradingCard2.jsx
@@ -0,0 +1,42 @@
+
+function TradingCard(props) {
+  return (
+    <div className="card">
+      <p> Name: {props.name} </p>
+      <img src={props.imgUrl} />
+      <p> Skill: {props.skill} </p>
+    </div>
+  );
+}
+
+function TradingCardContainer() {
+
+  const [cards, updateCards] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((data) => updateCards(data))
+  }, [])
+
+  const tradingCards = [];
+
+  for (const currentCard of cards) {
+    tradingCards.push(
+      <TradingCard
+        key={currentCard.name}
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl={currentCard.imgUrl}
+      />
+    );
+  }
+
+  return (
+    <div>{tradingCards}</div>
+  );
+
+}
+
+
+ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
